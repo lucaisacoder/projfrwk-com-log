8ab8a7df127aba36374acce4e5bf374a7967b567
lucaisacoder <lucas_hsueh@hotmail.com>
2020-02-03 23:02:55 +0800

    update Kconfig
    Change-Id: Id61f3cada4004427a649ea12599815bbe6dec7ab

--- code/src/log_api.c
+++ code/src/log_api.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "log.h"
+#include "local_kconfig.h"
+#include "global_kconfig.h"
 
 int log_api()
 {
 	printf("COM: log, FUNCTION:%s, VERSION:%d\n", __FUNCTION__, COM_LOG_VERSION);
+#ifdef COM_LOG_DEFINITION
+	printf("KCONFIG: enable\n");
+#endif
 	return 0;
 }
